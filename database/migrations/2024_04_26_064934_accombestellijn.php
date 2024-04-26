2d8cb90a4b9252a965390362327805513989937b
milan reijmer <milan.reijmer@student.graafschapcollege.nl>
2024-04-26 09:08:49 +0200

    typfout verbeterd in accombestellijn

--- database/migrations/2024_04_26_064934_accombestellijn.php
+++ database/migrations/2024_04_26_064934_accombestellijn.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->integer('BestelId');
-            $table->integer('AccomodatietId');
+            $table->integer('AccomodatieId');
         });
     }
 
